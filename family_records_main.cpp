c49e4ab3349560dbc53f4620473d8e5ca8d16400
Isaac Harrison <sateonlineservices@gmail.com>
2024-08-31 20:20:01 +0200

    added family records project

---
+++ family_records_main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+//person definition. Each person must follow this structure
+struct person_details{
+    string full_name;
+    int age;
+    bool leader;
+    string education_level;
+    bool working;
+    string family;
+    vector<string> suspected_genetic_illnesses;
+};
+
+
+
+
+int main(void)
+{
+    return 0;
+}
