abd798964c18abc813e8d8015f65e6602d43002c
Isaac Harrison <sateonlineservices@gmail.com>
2024-08-31 20:39:26 +0200

    added the person details structure

--- family_records_main.cpp
+++ family_records_main.cpp
@@ -11,8 +11,12 @@ struct person_details{
     string education_level;
     bool working;
     string family;
-    vector<string> suspected_genetic_illnesses;
+    vector<string> suspected_genetic_illnesses;  //every person of the same family will have similar genetic data attached as a vector just in case they can be more than one and for dynamic behaviour of vector as opposed to array
 };
+
+struct Family_details{
+    
+}
 
 
 
