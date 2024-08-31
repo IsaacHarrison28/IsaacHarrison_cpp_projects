fcc7b5470549c2baf26c0fd2095ceb4ab34be2f9
Isaac Harrison <sateonlineservices@gmail.com>
2024-08-31 22:06:19 +0200

    added family addition logic

--- family_records_main.cpp
+++ family_records_main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
 using namespace std;
+
+//ANSI definition of some colors used in the project
+#define BLUE    "\033[34m"
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
 
 //person definition. Each person must follow this structure
 struct person_details{
@@ -15,8 +21,52 @@ struct person_details{
 };
 
 struct Family_details{
+  string family_name;
+  int family_size;
+  vector<string> family_genetic_diseases; 
+};
+
+
+//function that handles the family addition logic
+void add_family_genetic_disease(vector<string>& diseases_record){
+    string user_choice;
+    string disease;
     
-}
+    do{
+        cout << "Enter a disease: ";
+        cin.ignore();
+        getline(cin, disease);
+        diseases_record.push_back(disease);
+        cout << "\nDisease successfully recorded." << GREEN << endl;
+        cout << "Is there another disease you want to add? (Y, N) ";
+        cin >> user_choice;
+    } while(user_choice[0] == 'Y' || user_choice[0] == 'y');
+};
+
+class Family{
+    public:
+    Family_details family;
+        int setFamily_details(){
+            cout << "We advice that you provide as accurate information as possible below: " << BLUE << endl;
+            cout << "What is the name of the family? ";
+            getline(cin, family.family_name);
+            cout << " " << endl;
+            cout << "How many members does the family currently have? ";
+            cin.ignore();
+            cin >> family.family_size;
+            cout << " " << endl;
+            cout << "Does any of the family members have a genetic disease? (Yes, No) ";
+            string user_choice;
+            cin.ignore();
+            cin >> user_choice;
+            cout << " " << endl;
+
+            //check if the first letter is "N" or "n" for "NO" or "Y" or "y" for "YES"
+            if(user_choice[0] == 'Y' || user_choice[0] == 'y'){
+                add_family_genetic_disease(family.family_genetic_diseases);
+            }
+        }
+};
 
 
 
